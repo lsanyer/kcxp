6a58cb4fd220188c398caa849e60f105409ff683
luotao <Administrator@sanyer-PC>
2017-07-19 14:23:28 +0800

    20170719-14:23

--- src/com/demo/NewDemo.java
+++ src/com/demo/NewDemo.java
@@ -8,10 +8,10 @@ import com.spasvo.kcbp.*;
 public class NewDemo {
 	KCBPBusiness kcbpBusiness = KCBPBusiness.getInstance();
 	String Excpath = "f:\\demo.xls";
-	/*
-  @Test//用例1资金查询校验数据
-  public void testcase1() throws Exception {
-	  Printname.printname("NewDemo_testcase1");
+
+  @Test//用例001资金查询校验数据
+  public void testcase001() throws Exception {
+	  Printname.printname("NewDemo_testcase001");
 	  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase1",1);
 		kcbpBusiness.fundQueryBusiness(paramMap);
 		String custid = KCBPBusiness.getStr(paramMap,"custid");
@@ -24,9 +24,9 @@ public class NewDemo {
 			AssertJUnit.fail("资金查询校验数据用例执行失败");}
   }
   
-  @Test//用例2股份查询校验数据
-  public void testcase2() throws Exception {
-	  Printname.printname("NewDemo_testcase2");
+  @Test//用例002股份查询校验数据
+  public void testcase002() throws Exception {
+	  Printname.printname("NewDemo_testcase002");
 	  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase1",2);
 	  	String stkcode = KCBPBusiness.getStr(paramMap,"stkcode");
 		kcbpBusiness.fundQueryBusiness(paramMap);
@@ -41,9 +41,9 @@ public class NewDemo {
   }
   
 	
-	 @Test//用例3股票正常委托买入
-	  public void testcase3() throws Exception {
-		  	Printname.printname("NewDemo_testcase3");
+	 @Test//用例003股票正常委托买入
+	  public void testcase003() throws Exception {
+		  	Printname.printname("NewDemo_testcase003");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase1",0);
 			kcbpBusiness.fundQueryBusiness(paramMap);
 			if(KCBPBusiness.Checkpoint(paramMap)){
@@ -51,42 +51,42 @@ public class NewDemo {
 			}
 	  }
 
-	  @Test//用例4股票零股买入
-	  public void testcase4() throws Exception {
-		  	Printname.printname("NewDemo_testcase4");
+	  @Test//用例004股票零股买入
+	  public void testcase004() throws Exception {
+		  	Printname.printname("NewDemo_testcase004");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase2",0);
 			kcbpBusiness.fundQueryBusiness(paramMap);
 			if(KCBPBusiness.Checkpoint(paramMap)){
 				AssertJUnit.fail("零股买入用例执行失败");}
 	  }
 
-	  @Test//用例5股票涨停价委托买入
-	  public void testcase5() throws Exception {
-		  	Printname.printname("NewDemo_testcase5");
+	  @Test//用例005股票涨停价委托买入
+	  public void testcase005() throws Exception {
+		  	Printname.printname("NewDemo_testcase005");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase3",0);
 			kcbpBusiness.fundQueryBusiness(paramMap);
 			if(KCBPBusiness.Checkpoint(paramMap)){
 				AssertJUnit.fail("涨停价委托买入用例执行失败");}
 	  }
-	  @Test//用例6股票跌停价委托买入
-	  public void testcase6() throws Exception {
-		  	Printname.printname("NewDemo_testcase6");
+	  @Test//用例006股票跌停价委托买入
+	  public void testcase006() throws Exception {
+		  	Printname.printname("NewDemo_testcase006");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase4",0);
 			kcbpBusiness.fundQueryBusiness(paramMap);
 			if(KCBPBusiness.Checkpoint(paramMap)){
 				AssertJUnit.fail("跌停价委托买入用例执行失败");}
 	  }
-	  @Test//用例7股票超涨停价委托买入
-	  public void testcase7() throws Exception {
-		  	Printname.printname("NewDemo_testcase7");
+	  @Test//用例007股票超涨停价委托买入
+	  public void testcase007() throws Exception {
+		  	Printname.printname("NewDemo_testcase007");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase5",0);
 			kcbpBusiness.fundQueryBusiness(paramMap);
 			if(KCBPBusiness.Checkpoint(paramMap)){
 				AssertJUnit.fail("超涨停价委托买入用例执行失败");}
 	  }
-	  @Test//用例8股票低跌停价委托买入
-	  public void testcase8() throws Exception {
-		  	Printname.printname("NewDemo_testcase8");
+	  @Test//用例008股票低跌停价委托买入
+	  public void testcase008() throws Exception {
+		  	Printname.printname("NewDemo_testcase008");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase6",0);
 			kcbpBusiness.fundQueryBusiness(paramMap);
 			if(KCBPBusiness.Checkpoint(paramMap)){
@@ -94,9 +94,9 @@ public class NewDemo {
 	  }
 	
 	
-	@Test//用例9股票正常委托卖出
-	  public void testcase9() throws Exception {
-		  	Printname.printname("NewDemo_testcase9");
+	@Test//用例009股票正常委托卖出
+	  public void testcase009() throws Exception {
+		  	Printname.printname("NewDemo_testcase009");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase7",0);
 			kcbpBusiness.fundQueryBusiness(paramMap);
 			if(KCBPBusiness.Checkpoint(paramMap)){
@@ -104,42 +104,42 @@ public class NewDemo {
 			}
 	  }
 
-	  @Test//用例10股票涨停价委托卖出
-	  public void testcase10() throws Exception {
-		  	Printname.printname("NewDemo_testcase10");
+	  @Test//用例010股票涨停价委托卖出
+	  public void testcase010() throws Exception {
+		  	Printname.printname("NewDemo_testcase010");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase8",0);
 			kcbpBusiness.fundQueryBusiness(paramMap);
 			if(KCBPBusiness.Checkpoint(paramMap)){
 				AssertJUnit.fail("涨停价委托卖出用例执行失败");}
 	  }
-	  @Test//用例11股票跌停价委托卖出
-	  public void testcase11() throws Exception {
-		  	Printname.printname("NewDemo_testcase11");
+	  @Test//用例011股票跌停价委托卖出
+	  public void testcase011() throws Exception {
+		  	Printname.printname("NewDemo_testcase011");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase9",0);
 			kcbpBusiness.fundQueryBusiness(paramMap);
 			if(KCBPBusiness.Checkpoint(paramMap)){
 				AssertJUnit.fail("跌停价委托卖出用例执行失败");}
 	  }
-	  @Test//用例12股票超涨停价委托卖出
-	  public void testcase12() throws Exception {
-		  	Printname.printname("NewDemo_testcase12");
+	  @Test//用例012股票超涨停价委托卖出
+	  public void testcase012() throws Exception {
+		  	Printname.printname("NewDemo_testcase012");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase10",0);
 			kcbpBusiness.fundQueryBusiness(paramMap);
 			if(KCBPBusiness.Checkpoint(paramMap)){
 				AssertJUnit.fail("超涨停价委托卖出用例执行失败");}
 	  }
-	  @Test//用例13股票低跌停价委托卖出
-	  public void testcase13() throws Exception {
-		  	Printname.printname("NewDemo_testcase13");
+	  @Test//用例013股票低跌停价委托卖出
+	  public void testcase013() throws Exception {
+		  	Printname.printname("NewDemo_testcase013");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase11",0);
 			kcbpBusiness.fundQueryBusiness(paramMap);
 			if(KCBPBusiness.Checkpoint(paramMap)){
 				AssertJUnit.fail("低跌停价委托卖出用例执行失败");}
 	  }
-	*/  
-	@Test//用例14股票零股卖出-零股卖零（50-50）
-	  public void testcase14() throws Exception {
-		  	Printname.printname("NewDemo_testcase14");
+ 
+	@Test//用例014股票零股卖出-零股卖零（50-50）
+	  public void testcase014() throws Exception {
+		  	Printname.printname("NewDemo_testcase014");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase12",0);
 			String custid = KCBPBusiness.getStr(paramMap,"custid");
 		  	String stkcode = KCBPBusiness.getStr(paramMap,"stkcode");
@@ -149,9 +149,9 @@ public class NewDemo {
 			if(KCBPBusiness.Checkpoint(paramMap)){
 				AssertJUnit.fail("股票零股卖出用例执行失败");}
 	  }
-	@Test//用例15股票零股卖出-零股卖零（50-25）
-	  public void testcase15() throws Exception {
-		  	Printname.printname("NewDemo_testcase15");
+	@Test//用例015股票零股卖出-零股卖零（50-25）
+	  public void testcase015() throws Exception {
+		  	Printname.printname("NewDemo_testcase015");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase13",0);
 		  	String custid = KCBPBusiness.getStr(paramMap,"custid");
 		  	String stkcode = KCBPBusiness.getStr(paramMap,"stkcode");
@@ -161,9 +161,9 @@ public class NewDemo {
 			if(KCBPBusiness.Checkpoint(paramMap)){
 				AssertJUnit.fail("股票零股卖出用例执行失败");}
 	  }
-	@Test//用例16股票零股卖出-零股卖零（50-25）
-	  public void testcase16() throws Exception {
-		  	Printname.printname("NewDemo_testcase16");
+	@Test//用例016股票零股卖出-零股卖零（50-25）
+	  public void testcase016() throws Exception {
+		  	Printname.printname("NewDemo_testcase016");
 		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase14",0);
 		  	String custid = KCBPBusiness.getStr(paramMap,"custid");
 		  	String stkcode = KCBPBusiness.getStr(paramMap,"stkcode");
@@ -175,31 +175,215 @@ public class NewDemo {
 	  }
 	
 	
+
+	 @Test//用例017国债正常委托买入
+	  public void testcase017() throws Exception {
+		  	Printname.printname("NewDemo_testcase017");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase1",4);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("国债委托买入用例执行失败");
+			}
+	  }
+	
+	 @Test//用例018国债零股委托买入
+	  public void testcase018() throws Exception {
+		  	Printname.printname("NewDemo_testcase018");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase2",4);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("国债零股买入用例执行失败");
+			}
+	  }
+	
+	 @Test//用例019国债正常委托卖出
+	  public void testcase019() throws Exception {
+		  	Printname.printname("NewDemo_testcase019");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase3",4);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("国债委托卖出用例执行失败");
+			}
+	  }
+	
+	 @Test//用例020国债零股卖出-零股卖零（5-5）
+	  public void testcase020() throws Exception {
+		  	Printname.printname("NewDemo_testcase020");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase4",4);
+		  	String custid = KCBPBusiness.getStr(paramMap,"custid");
+		  	String stkcode = KCBPBusiness.getStr(paramMap,"stkcode");
+		  	String Sql = "update run..stkasset set stklastbal = '10000005',stkbal = '10000005',stkavl = '10000005' where custid = \'"+custid+"\' and stkcode = \'"+stkcode+"\'";
+		  	SqlHelper.executeUpdate(Sql);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("国债零股卖出-零股卖零（5-5）用例执行失败");
+			}
+	  }
+	
+	 @Test//用例021国债零股卖出-零股卖零（5-4）
+	  public void testcase021() throws Exception {
+		  	Printname.printname("NewDemo_testcase021");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase5",4);
+		  	String custid = KCBPBusiness.getStr(paramMap,"custid");
+		  	String stkcode = KCBPBusiness.getStr(paramMap,"stkcode");
+		  	String Sql = "update run..stkasset set stklastbal = '10000005',stkbal = '10000005',stkavl = '10000005' where custid = \'"+custid+"\' and stkcode = \'"+stkcode+"\'";
+		  	SqlHelper.executeUpdate(Sql);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("国债零股卖出-零股卖零（5-4）用例执行失败");
+			}
+	  }
+	
+	 @Test//用例022国债零股卖出-整股卖零
+	  public void testcase022() throws Exception {
+		  	Printname.printname("NewDemo_testcase022");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase6",4);
+		  	String custid = KCBPBusiness.getStr(paramMap,"custid");
+		  	String stkcode = KCBPBusiness.getStr(paramMap,"stkcode");
+		  	String Sql = "update run..stkasset set stklastbal = '10000010',stkbal = '10000010',stkavl = '10000010' where custid = \'"+custid+"\' and stkcode = \'"+stkcode+"\'";
+		  	SqlHelper.executeUpdate(Sql);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("国债零股卖出-整股卖零用例执行失败");
+			}
+	  }
+
+	 @Test//用例023基金正常委托买入
+	  public void testcase023() throws Exception {
+		  	Printname.printname("NewDemo_testcase023");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase1",5);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金正常委托买入用例执行失败");
+			}
+	  }
+
+	  @Test//用例024基金零股买入
+	  public void testcase024() throws Exception {
+		  	Printname.printname("NewDemo_testcase024");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase2",5);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金零股买入用例执行失败");}
+	  }
+
+	  @Test//用例025基金涨停价委托买入
+	  public void testcase025() throws Exception {
+		  	Printname.printname("NewDemo_testcase025");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase3",5);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金涨停价委托买入用例执行失败");}
+	  }
+	  @Test//用例026基金跌停价委托买入
+	  public void testcase026() throws Exception {
+		  	Printname.printname("NewDemo_testcase026");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase4",5);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金跌停价委托买入用例执行失败");}
+	  }
+	  @Test//用例027基金超涨停价委托买入
+	  public void testcase027() throws Exception {
+		  	Printname.printname("NewDemo_testcase027");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase5",5);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金超涨停价委托买入入用例执行失败");}
+	  }
+	  @Test//用例028基金低跌停价委托买入
+	  public void testcase028() throws Exception {
+		  	Printname.printname("NewDemo_testcase028");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase6",5);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金低跌停价委托买入用例执行失败");}
+	  }
 	
 	
+	@Test//用例029基金正常委托卖出
+	  public void testcase029() throws Exception {
+		  	Printname.printname("NewDemo_testcase029");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase7",5);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金正常委托卖出用例执行失败");
+			}
+	  }
+
+	  @Test//用例030基金涨停价委托卖出
+	  public void testcase030() throws Exception {
+		  	Printname.printname("NewDemo_testcase030");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase8",5);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金涨停价委托卖出用例执行失败");}
+	  }
+	  @Test//用例031基金跌停价委托卖出
+	  public void testcase031() throws Exception {
+		  	Printname.printname("NewDemo_testcase031");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase9",5);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金跌停价委托卖出用例执行失败");}
+	  }
+	  @Test//用例032基金超涨停价委托卖出
+	  public void testcase032() throws Exception {
+		  	Printname.printname("NewDemo_testcase032");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase10",5);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金超涨停价委托卖出用例执行失败");}
+	  }
+	  @Test//用例033基金低跌停价委托卖出
+	  public void testcase033() throws Exception {
+		  	Printname.printname("NewDemo_testcase033");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase11",5);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金低跌停价委托卖出用例执行失败");}
+	  }
+
+	@Test//用例034基金零股卖出-零股卖零（50-50）
+	  public void testcase034() throws Exception {
+		  	Printname.printname("NewDemo_testcase034");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase12",5);
+			String custid = KCBPBusiness.getStr(paramMap,"custid");
+		  	String stkcode = KCBPBusiness.getStr(paramMap,"stkcode");
+		  	String Sql = "update run..stkasset set stklastbal = '10000050',stkbal = '10000050',stkavl = '10000050' where custid = \'"+custid+"\' and stkcode = \'"+stkcode+"\'";
+		  	SqlHelper.executeUpdate(Sql);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("用例34基金零股卖出-零股卖零（50-50）用例执行失败");}
+	  }
+	@Test//用例035基金零股卖出-零股卖零（50-25）
+	  public void testcase035() throws Exception {
+		  	Printname.printname("NewDemo_testcase035");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase13",5);
+		  	String custid = KCBPBusiness.getStr(paramMap,"custid");
+		  	String stkcode = KCBPBusiness.getStr(paramMap,"stkcode");
+		  	String Sql = "update run..stkasset set stklastbal = '10000050',stkbal = '10000050',stkavl = '10000050' where custid = \'"+custid+"\' and stkcode = \'"+stkcode+"\'";
+		  	SqlHelper.executeUpdate(Sql);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金零股卖出-零股卖零（50-25）用例执行失败");}
+	  }
+	@Test//用例036基金零股卖出-零股卖零（50-25）
+	  public void testcase036() throws Exception {
+		  	Printname.printname("NewDemo_testcase036");
+		  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase14",5);
+		  	String custid = KCBPBusiness.getStr(paramMap,"custid");
+		  	String stkcode = KCBPBusiness.getStr(paramMap,"stkcode");
+		  	String Sql = "update run..stkasset set stklastbal = '10000000',stkbal = '10000000',stkavl = '10000000' where custid = \'"+custid+"\' and stkcode = \'"+stkcode+"\'";
+		  	SqlHelper.executeUpdate(Sql);
+			kcbpBusiness.fundQueryBusiness(paramMap);
+			if(KCBPBusiness.Checkpoint(paramMap)){
+				AssertJUnit.fail("基金零股卖出-零股卖零（50-25）用例执行失败");}
+	  }
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-/*	
-  @Test//用例3委托查询校验数据
-  public void testcase3() throws Exception {
-	  Printname.printname("NewDemo_testcase3");
+  @Test//用例037委托查询校验数据
+  public void testcase037() throws Exception {
+	  Printname.printname("NewDemo_testcase037");
 	  	Map<String, String> paramMap = ReadExc.Readcase(Excpath,"testcase1",3);
 		kcbpBusiness.fundQueryBusiness(paramMap);
 		if(!paramMap.containsKey("ordersno")){
@@ -216,5 +400,5 @@ public class NewDemo {
 				AssertJUnit.fail("委托查询用例执行失败");
 			}
 		}
-  }*/
+  }
 }
